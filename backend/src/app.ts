3855c93e09c522a2a3084c99f64095f2ec95e942
MaYnk0018 <126932719+MaYnk0018@users.noreply.github.com>
2025-01-10 17:41:28 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -28,7 +28,7 @@ const app: Express = express();
 app.use(helmet()); // Security headers
 app.use(compression()); // Compress responses
 app.use(cors({
-  origin: 'https://localhost:3000'  // Frontend URL
+  origin: 'https://hospital-food-management-eight.vercel.app/login'  // Frontend URL
 }));
 app.use(express.json()); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
